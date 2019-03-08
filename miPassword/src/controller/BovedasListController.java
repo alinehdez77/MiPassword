3f1d3c01d38dd11634763567d017380d2ad56d1c
Carlos Carrillo <texcharly9@gmail.com>
2019-03-07 21:43:59 -0600

    Creación de la GUI referente a las bóvedas

---
+++ miPassword/src/controller/BovedasListController.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author texch
+ */
+public class BovedasListController implements Initializable {
+
+  @FXML
+  private AnchorPane anchorPane;
+  @FXML
+  private BorderPane borderPane;
+  @FXML
+  private SplitPane splitPane;
+  @FXML
+  private ListView<String> listaBovedas;
+  @FXML
+  private Label lblUsername;
+  @FXML
+  private Button btnNuevaBoveda;
+  @FXML
+  private Button btnRegresar;
+  @FXML
+  private Button btnNuevaLlave;
+
+  /**
+   * Initializes the controller class.
+   */
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    // TODO
+  }  
+  
+}
